cf22a3d2bd5de0be01d8d331e207ce50c0fb3f33
venkatsai2027 <venkatsai2027@gmail.com>
2024-07-12 11:41:22 +0530

    files are added

---
+++ src/components/main/main.jsx
@@ -0,0 +1,65 @@
+import './main.css'
+
+const Main = () => {
+	return (
+		<div className = 'div'>
+			<h1>This week list dated 13-07-2024</h1>
+			<div className = 'content'>
+				<ol>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name1</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name2</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name3</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name4</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name5</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name6</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name7</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name8</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name9</span>
+						<p>Description lorem24</p>
+					</li>
+					<li>
+						<img src = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg" alt = "" />
+						<span>Name10</span>
+						<p>Description lorem24</p>
+					</li>
+				</ol>
+			</div>
+		</div>
+	)
+}
+
+export default Main
